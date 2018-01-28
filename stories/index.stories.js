f8bbb6b7de66e167c7c18d7dad66922a3d55d32b
Gregory Starr <gregory.a.starr@gmail.com>
2018-01-28 01:53:29 -0600

    re-adding files

---
+++ stories/index.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import Video from '../src/components/video'
+import { Button, Welcome } from '@storybook/react/demo';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('VideoPlayer', module)
+    .add('unstyled', () => <Video type="mp4" src="video.mp4" controls></Video>)
+
